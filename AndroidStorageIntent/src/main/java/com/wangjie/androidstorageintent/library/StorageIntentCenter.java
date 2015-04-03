6c1642167f71e8e12f25230b27ae5732b9663974
王杰 <tiantian.china.2@gmail.com>
2015-04-03 12:11:55 +0800

    禁止StorageIntentCenter被reuse

--- AndroidStorageIntent/src/main/java/com/wangjie/androidstorageintent/library/StorageIntentCenter.java
+++ AndroidStorageIntent/src/main/java/com/wangjie/androidstorageintent/library/StorageIntentCenter.java
@@ -2,6 +2,7 @@ package com.wangjie.androidstorageintent.library;
 
 import android.content.Context;
 import android.content.Intent;
+import com.wangjie.androidbucket.log.Logger;
 import com.wangjie.androidbucket.utils.ABTextUtil;
 
 import java.util.HashMap;
@@ -15,14 +16,17 @@ import java.util.Set;
  */
 public class StorageIntentCenter {
     public static final String STORAGE_INTENT_CENTER_KEY_UUID = StorageIntentCenter.class.getSimpleName() + "_UUID";
+    private static final String TAG = StorageIntentCenter.class.getSimpleName();
 
     private Intent intent;
     private String uuid;
     private HashMap<StorageKey, Object> extras;
+    private boolean isUsed;
     public StorageIntentCenter() {
         intent = new Intent();
         uuid = java.util.UUID.randomUUID().toString();
         intent.putExtra(STORAGE_INTENT_CENTER_KEY_UUID, uuid);
+        isUsed = false;
     }
 
     public StorageIntentCenter putExtra(String intentKey, Object content){
@@ -39,6 +43,10 @@ public class StorageIntentCenter {
     }
 
     public void startActivity(Context packageContext, Class<?> cls){
+        if(isUsed){
+            Logger.e(TAG, this + " can not be reuse!");
+            return;
+        }
         intent.setClass(packageContext, cls);
         if(!ABTextUtil.isEmpty(extras)){
             Set<Map.Entry<StorageKey, Object>> entrySet = extras.entrySet();
@@ -46,6 +54,7 @@ public class StorageIntentCenter {
                 StoragePool.storage(uuid, entry.getKey(), entry.getValue());
             }
         }
+        isUsed = true;
         packageContext.startActivity(intent);
     }
 
